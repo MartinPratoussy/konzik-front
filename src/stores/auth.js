6296820ade3d77872893981c249dba6498e5c83f
Harutiun <harutiun.hovsepyan@gmail.com>
2023-01-15 20:36:12 +0100

    add event request

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -77,13 +77,30 @@ export const useAuthStore = defineStore({
     
         // Add new event in the store
         reloadUserEvents() {
-            // Clear
+            // Clean
             this.userEvents = []
     
             // Add
-            this.addUserEvent(101, "11/10/2022", "Sting", "Pop", "Le fil", "Saint-Etienne", "France")
-            this.addUserEvent(102, "08/12/2022", "Gazo", "Rap", "Transbo", "Lyon", "France")
-            this.addUserEvent(101, "11/10/2022", "Sting", "Pop", "Le fil", "Saint-Etienne", "France")
+            // this.addUserEvent(101, "11/10/2022", "Sting", "Pop", "Le fil", "Saint-Etienne", "France")
+            // this.addUserEvent(102, "08/12/2022", "Gazo", "Rap", "Transbo", "Lyon", "France")
+            // this.addUserEvent(101, "11/10/2022", "Sting", "Pop", "Le fil", "Saint-Etienne", "France")
+
+            var config = {
+                method: 'get',
+                url: 'api.descours.cc/CONCERT-SERVICE/api/concert/all',
+                headers: { 
+                    'Authorization': 'Bearer ' + localStorage.getItem("token"),
+                    'Content-Type': 'application/json'
+                },
+            };
+
+            axios(config)
+                .then((response) => {
+                    response.data.events.forEach(el => this.addEvent(el.idEvent, el.date, el.artist, el.genre, el.location, el.city, el.country))
+                })
+                .catch(function (error) {
+                console.log(error);
+            });
     
             // let fetched_response = {}
             // axios.get('localhost/').then((response) => {console.log(response), fetched_response = response})
